f6fb0f687c1f45ad02c09e161078b5722a24257b
Breign3 <brainy.ngwenya@gmail.com>
2024-06-18 05:40:20 -0700

    134-heap_to_extract.c

--- 131-main.c
+++ 131-main.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-    heap_t *root;
-    heap_t *node;
+	heap_t *root;
+	heap_t *node;
 
-    root = NULL;
-    node = heap_insert(&root, 98);
+	root = NULL;
+	node = heap_insert(&root, 98);
 	printf("Inserted: %d\n", node->n);
 	binary_tree_print(root);
 	node = heap_insert(&root, 402);
